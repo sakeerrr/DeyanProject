6d317a5dae0b8d2c2c1ffaa83c54ea946a437b4b
sakeerrr <bkurtev@abv.bg>
2026-01-29 13:07:18 +0200

    .env changes

--- bookshop/db.js
+++ bookshop/db.js
@@ -1,10 +1,12 @@
 require("dotenv").config();  
+console.log("DB_PASS =", process.env.PASS);
+
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: process.PASS,   // <-- HERE
+  password: process.PASS,
   database: "bookshop",
   waitForConnections: true,
   connectionLimit: 10
